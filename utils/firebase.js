4ade7711f5ae1cceb54923410b0dcd3b3a1fbafb
nahidnstu12 <nahid.dev19@gmail.com>
2022-05-13 10:31:54 +0600

    firebase init

---
+++ utils/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCFjsgU2djQdOYjS1Z9Ms9S-vM2a2QoFaY",
+  authDomain: "food-finding-90589.firebaseapp.com",
+  projectId: "food-finding-90589",
+  storageBucket: "food-finding-90589.appspot.com",
+  messagingSenderId: "498203756048",
+  appId: "1:498203756048:web:1dd27436621326334e6c95",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
